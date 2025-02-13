17bbaa76479865f68ae6dfb9a5efd0a80e7dbf12
Moyartu Manley <mmanley@macalester.edu>
2025-02-13 03:44:33 +0000

    Updates to documentation

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
-/*
- * Test of scramble function as defined in scramble.c
+/* Author: Moyartu Manley
+ * Test of scramble functions as defined in scramble.c
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 	char *scrambled_bin_str;
 	int expected;
 
+	//---------------------------------------------------Cases in which the scrambled result is the same---------------------------------------------------
 	// Test Case 1: Binary string of all zeros
 	input = 0x00000000;
 	input_bin_str = int_to_bin_str(input);
@@ -35,12 +36,11 @@ int main(int argc, char *argv[])
 	free(input_bin_str);
 	free(scrambled_bin_str);
 
-
 	// Test Case 2: Binary string of all ones
 	input = 0xFFFFFFFF;
 	input_bin_str = int_to_bin_str(input);
 
-	scrambled_input = scramble(input); //masks should be the same as their original due to input consisting of ones
+	scrambled_input = scramble(input); // masks should be the same as their original due to input consisting of ones
 	scrambled_bin_str = int_to_bin_str(scrambled_input);
 
 	// Expected outcome:
@@ -54,7 +54,6 @@ int main(int argc, char *argv[])
 	// Freeing memory:
 	free(input_bin_str);
 	free(scrambled_bin_str);
-
 
 	// Test Case 3: Alternating bits (1 first)
 	input = 0xAAAAAAAA;
@@ -94,6 +93,7 @@ int main(int argc, char *argv[])
 	free(input_bin_str);
 	free(scrambled_bin_str);
 
+	//---------------------------------------------------Cases in which the scrambled result is different---------------------------------------------------
 	// Test Case 5: Non-repeating nibbles
 	input = 0xABCD1234;
 	input_bin_str = int_to_bin_str(input);
@@ -113,5 +113,4 @@ int main(int argc, char *argv[])
 	free(input_bin_str);
 	free(scrambled_bin_str);
 	return 0;
-
 }
